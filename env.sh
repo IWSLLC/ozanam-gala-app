60bd42e5279f8a6359125df4527a1af75fdcb677
Nathan Bridgewater <nathan@integratedwebsystems.com>
2013-10-06 13:08:40 -0500

    work in progress

--- env.sh
+++ env.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -e 
 
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_ACCESS_KEY=
 export PORT=3000
 export PORTSSL=8000
 export ENABLESSL=no
 export SSL_HOSTNAME=localhost:8000
-export ASSETS_HOSTNAME=
 export OCRYPTO=pass
 export NODE_ENV=development
 export MONGO="mongodb://localhost:27017/gala?auto_reconnect=true&maxPoolSize=10"
+export PP_HOST="api.sandbox.paypal.com"
+export PP_PORT=""
+export PP_CLIENT_ID="AaLRPRARNlKIAUsZ3qyYlJlcuo3QRJzGHA8h6a0LDkrnJJIKdV2RoOUpcgz4"
+export PP_CLIENT_SECRET="ECTJxRDiMzI89Pg91bP0Qq52IMGtDRccEFfgLn1nmkB6HHWb8w5Z3DFUOlz7"
