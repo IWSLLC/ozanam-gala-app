bdbdd0e92eb6a895c882d9d35bc57d1a7614418d
Nathan Bridgewater <nathan@integratedwebsystems.com>
2013-10-21 11:53:26 -0500

    fixed title... doh

--- routes/public.js
+++ routes/public.js
@@ -3,7 +3,7 @@ var vm = require('../lib/vm.js')
 module.exports = function (app, auth) {
   app.get('/', function(req, res) {
     var model = vm.new();
-    model.title = 'Welcome to the 2013 Ozanam Holywood Holiday Gala'
+    model.title = 'Welcome to the 2013 Ozanam Hollywood Holiday Gala'
     model.titlesuffix = false
     return res.render('index.html', model)
   })
