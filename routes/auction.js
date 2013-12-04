2258ee874b4e6f1c3a608ea5492e773784027310
Nathan Bridgewater <nathan@integratedwebsystems.com>
2013-12-03 21:11:03 -0600

    disabling auction and registration for 2013

--- routes/auction.js
+++ routes/auction.js
@@ -18,83 +18,84 @@ module.exports = function (app, auth) {
       })
   }
 
-  app.get('/auction/thankyou', function(req, res) {
-    var model = vm.new();
-    model.title = 'Thankyou for registering your donation!'
-    return res.render('donationThankyou.html', model)
-  })
-
   app.get('/auction', function(req,res) {
     ocrypto.httpsCheck(req,res)
     var model = vm.new();
     model.title = 'Donate Auction Item'
-    return res.render('donate.html', model)
+    return res.render('donate-disabled.html', model)
+    //return res.render('donate.html', model)
   })
-  
-  app.post('/auction', function(req, res) { 
-    ocrypto.httpsCheck(req,res)
-    var post = req.body;
-    var worked = true;
 
-    var broke = []
-    if (!((post.name || '').trim()))
-      broke.push({field: 'name', message: 'required'})
-    if (!((post.contact || '').trim()))
-      broke.push({field: 'contact', message: 'required'})
-    if (!((post.street || '').trim()))
-      broke.push({field: 'street', message: 'required'})
-    if (!((post.city || '').trim()))
-      broke.push({field: 'city', message: 'required'})
-    if (!((post.state || '').trim()))
-      broke.push({field: 'state', message: 'required'})
-    if (!((post.zip || '').trim()))
-      broke.push({fdield: 'zip', message: 'required'})
-    if (!((post.phone || '').trim()))
-      broke.push({field: 'phone', message: 'required'})
-    if (!((post.itemDescription || '').trim()))
-      broke.push({field: 'itemDescription', message: 'required'})
+  // app.get('/auction/thankyou', function(req, res) {
+  //   var model = vm.new();
+  //   model.title = 'Thankyou for registering your donation!'
+  //   return res.render('donationThankyou.html', model)
+  // })
 
-    if (broke.length > 0) {
-      var model = {
-        success : false,
-        broke : broke
-      }
-      res.json(model)
-      return
-    }
+  // app.post('/auction', function(req, res) { 
+  //   ocrypto.httpsCheck(req,res)
+  //   var post = req.body;
+  //   var worked = true;
 
-    //convert to db record. 
+  //   var broke = []
+  //   if (!((post.name || '').trim()))
+  //     broke.push({field: 'name', message: 'required'})
+  //   if (!((post.contact || '').trim()))
+  //     broke.push({field: 'contact', message: 'required'})
+  //   if (!((post.street || '').trim()))
+  //     broke.push({field: 'street', message: 'required'})
+  //   if (!((post.city || '').trim()))
+  //     broke.push({field: 'city', message: 'required'})
+  //   if (!((post.state || '').trim()))
+  //     broke.push({field: 'state', message: 'required'})
+  //   if (!((post.zip || '').trim()))
+  //     broke.push({fdield: 'zip', message: 'required'})
+  //   if (!((post.phone || '').trim()))
+  //     broke.push({field: 'phone', message: 'required'})
+  //   if (!((post.itemDescription || '').trim()))
+  //     broke.push({field: 'itemDescription', message: 'required'})
 
-    var r = reg.new()
-    r.contact.donor = post.name
-    r.contact.company = post.company
-    r.contact.contact = post.contact
-    r.contact.street = post.street
-    r.contact.city = post.city
-    r.contact.state = post.state
-    r.contact.zip = post.zip
-    r.contact.phone = post.phone
-    r.contact.email = post.email
+  //   if (broke.length > 0) {
+  //     var model = {
+  //       success : false,
+  //       broke : broke
+  //     }
+  //     res.json(model)
+  //     return
+  //   }
 
-    r.item.description = post.itemDescription
-    r.item.value = parseFloat(post.itemValue)
-    r.item.restrictions = post.restrictions
-    r.item.optionDeliveredWithForm = post.optionDeliveredWithForm === "true"
-    r.item.optionSelfDelivery = post.optionSelfDelivery === "true"
-    if (r.item.optionSelfDelivery)
-      r.item.selfDeliveryDate = moment(post.selfDeliveryDate).toDate()
-    r.item.optionPickup = post.optionPickup === "true"
-    r.item.optionPrepareCertificate = post.optionPrepareCertificate === "true"
-    r.item.optionDisplayMaterials = post.optionDisplayMaterials === "true"
+  //   //convert to db record. 
 
-    reg.insert(r, function(err,data) {
-      if (err) {
-        res.redirect('/problem')
-      }
-      else {
-        sendNotification(data)
-        res.json({success: true, id : data._id.toHexString()})
-      }
-    });
-  })
+  //   var r = reg.new()
+  //   r.contact.donor = post.name
+  //   r.contact.company = post.company
+  //   r.contact.contact = post.contact
+  //   r.contact.street = post.street
+  //   r.contact.city = post.city
+  //   r.contact.state = post.state
+  //   r.contact.zip = post.zip
+  //   r.contact.phone = post.phone
+  //   r.contact.email = post.email
+
+  //   r.item.description = post.itemDescription
+  //   r.item.value = parseFloat(post.itemValue)
+  //   r.item.restrictions = post.restrictions
+  //   r.item.optionDeliveredWithForm = post.optionDeliveredWithForm === "true"
+  //   r.item.optionSelfDelivery = post.optionSelfDelivery === "true"
+  //   if (r.item.optionSelfDelivery)
+  //     r.item.selfDeliveryDate = moment(post.selfDeliveryDate).toDate()
+  //   r.item.optionPickup = post.optionPickup === "true"
+  //   r.item.optionPrepareCertificate = post.optionPrepareCertificate === "true"
+  //   r.item.optionDisplayMaterials = post.optionDisplayMaterials === "true"
+
+  //   reg.insert(r, function(err,data) {
+  //     if (err) {
+  //       res.redirect('/problem')
+  //     }
+  //     else {
+  //       sendNotification(data)
+  //       res.json({success: true, id : data._id.toHexString()})
+  //     }
+  //   });
+  // })
 }
--- routes/register.js
+++ routes/register.js
@@ -33,183 +33,184 @@ module.exports = function (app, auth) {
       })
   }
 
-  app.get('/register/thankyou', function(req, res) {
-    if (!req.query.confirm)
-      return res.redirect('/register')
-
-    reg.findById(req.query.confirm, function(err,doc) {
-      var model = vm.new();
-      model.title = 'Registration'
-      model.id = doc._id.toHexString()
-      model.amount = number.formatMoney(doc.order.total)
-      model.sponsorship = reg.getSponsorshipInfo(doc.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
-      model.donation = number.formatMoney(doc.order.donation)
-      model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(doc.order.extraSeats))
-      model.doc = doc
-      model.problem = req.query.problem || 0
-      return res.render('thankyou.html', model)
-    })
-  })
-
   //-----REGISTRATION-----
   app.get('/register', function(req, res) {
     ocrypto.httpsCheck(req,res)
     var model = vm.new()
     model.title = 'Registration'
-    return res.render('register.html', model)
+    return res.render('register-disabled.html', model)
+    //return res.render('register.html', model)
   })
 
-  app.get('/register/confirm', function(req,res) {
-    if (!req.query.confirm)
-      return res.redirect('/register')
+  // app.get('/register/thankyou', function(req, res) {
+  //   if (!req.query.confirm)
+  //     return res.redirect('/register')
 
-    reg.findById(req.query.confirm, function(err,doc) {
-      if (err || !doc) {
-        console.log(err || 'doc is not found: ' + req.query.confirm)
-        return res.redirect('/problem');
-      }
+  //   reg.findById(req.query.confirm, function(err,doc) {
+  //     var model = vm.new();
+  //     model.title = 'Registration'
+  //     model.id = doc._id.toHexString()
+  //     model.amount = number.formatMoney(doc.order.total)
+  //     model.sponsorship = reg.getSponsorshipInfo(doc.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
+  //     model.donation = number.formatMoney(doc.order.donation)
+  //     model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(doc.order.extraSeats))
+  //     model.doc = doc
+  //     model.problem = req.query.problem || 0
+  //     return res.render('thankyou.html', model)
+  //   })
+  // })
 
-      if (doc.confirmed) 
-        return res.redirect('/register/thankyou?confirm=' + req.query.confirm)
+  // app.get('/register/confirm', function(req,res) {
+  //   if (!req.query.confirm)
+  //     return res.redirect('/register')
 
-      var model = vm.new();
-      model.title = 'Registration'
-      model.id = doc._id.toHexString()
-      model.problem = req.query.problem || null
-      model.amount = number.formatMoney(doc.order.total)
-      model.nopaypal = doc.order.total > 10000
-      model.sponsorship = reg.getSponsorshipInfo(doc.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
-      model.donation = number.formatMoney(doc.order.donation)
-      model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(doc.order.extraSeats))
-      model.doc = doc
-      return res.render('confirm.html', model)
-    })
-  })
+  //   reg.findById(req.query.confirm, function(err,doc) {
+  //     if (err || !doc) {
+  //       console.log(err || 'doc is not found: ' + req.query.confirm)
+  //       return res.redirect('/problem');
+  //     }
 
-  app.post('/register/confirm', function(req,res) {
-    if (!req.body.confirmation)
-      return res.redirect('/register')
+  //     if (doc.confirmed) 
+  //       return res.redirect('/register/thankyou?confirm=' + req.query.confirm)
 
-    reg.findById(req.body.confirmation, function(err,doc) {
-      if (err || !doc) {
-        console.log(err || 'doc not found: ' + req.body.confirmation)
-        return res.redirect('/problem')
-      }
+  //     var model = vm.new();
+  //     model.title = 'Registration'
+  //     model.id = doc._id.toHexString()
+  //     model.problem = req.query.problem || null
+  //     model.amount = number.formatMoney(doc.order.total)
+  //     model.nopaypal = doc.order.total > 10000
+  //     model.sponsorship = reg.getSponsorshipInfo(doc.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
+  //     model.donation = number.formatMoney(doc.order.donation)
+  //     model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(doc.order.extraSeats))
+  //     model.doc = doc
+  //     return res.render('confirm.html', model)
+  //   })
+  // })
 
-      if (doc.confirmed)
-        return res.redirect('/register/thankyou?confirm=' + doc._id.toHexString())
+  // app.post('/register/confirm', function(req,res) {
+  //   if (!req.body.confirmation)
+  //     return res.redirect('/register')
 
-      if (req.body.paymentOption == 'paypal') {
-        pay.start(doc, function(err,redir) {
-          if (err) return handleError(err,doc,res)
-          return res.redirect(redir)
-        })
-      }
-      else {
-        reg.setConfirmation(doc._id)
-        sendNotification(doc)
-        return res.redirect('/register/thankyou?confirm=' + doc._id.toHexString())
-      }
-    })
-  })
+  //   reg.findById(req.body.confirmation, function(err,doc) {
+  //     if (err || !doc) {
+  //       console.log(err || 'doc not found: ' + req.body.confirmation)
+  //       return res.redirect('/problem')
+  //     }
 
-  app.get('/register/finish', function(req,res) {
-    if (!req.query.token || !req.query.PayerID)
-      return res.redirect('/register')
+  //     if (doc.confirmed)
+  //       return res.redirect('/register/thankyou?confirm=' + doc._id.toHexString())
+
+  //     if (req.body.paymentOption == 'paypal') {
+  //       pay.start(doc, function(err,redir) {
+  //         if (err) return handleError(err,doc,res)
+  //         return res.redirect(redir)
+  //       })
+  //     }
+  //     else {
+  //       reg.setConfirmation(doc._id)
+  //       sendNotification(doc)
+  //       return res.redirect('/register/thankyou?confirm=' + doc._id.toHexString())
+  //     }
+  //   })
+  // })
+
+  // app.get('/register/finish', function(req,res) {
+  //   if (!req.query.token || !req.query.PayerID)
+  //     return res.redirect('/register')
     
-    reg.setPayerId(req.query.token, req.query.PayerID, function(err,record) {
-      if (err) return handleError(err,record,res)
+  //   reg.setPayerId(req.query.token, req.query.PayerID, function(err,record) {
+  //     if (err) return handleError(err,record,res)
 
-      var model = vm.new()
-      model.title = 'Registration'
-      model.amount = number.formatMoney(record.order.total)
-      model.sponsorship = reg.getSponsorshipInfo(record.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
-      model.donation = number.formatMoney(record.order.donation)
-      model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(record.order.extraSeats))
-      model.id = record._id.toHexString()
-      return res.render('finish.html', model)
-    })
-  })
+  //     var model = vm.new()
+  //     model.title = 'Registration'
+  //     model.amount = number.formatMoney(record.order.total)
+  //     model.sponsorship = reg.getSponsorshipInfo(record.order.level) || {amount: 0.0, description : 'Extra Seats Only', level: 10}
+  //     model.donation = number.formatMoney(record.order.donation)
+  //     model.extraSeats = number.formatMoney(reg.getExtraSeatsAmount(record.order.extraSeats))
+  //     model.id = record._id.toHexString()
+  //     return res.render('finish.html', model)
+  //   })
+  // })
 
-  app.post('/register/finish', function(req,res) {
-    pay.finish(req.body.registrationId, function(err,record) {
-      if (err) return handleError(err,record,res)
+  // app.post('/register/finish', function(req,res) {
+  //   pay.finish(req.body.registrationId, function(err,record) {
+  //     if (err) return handleError(err,record,res)
 
-      reg.setConfirmation(record._id)
-      sendNotification(record)
+  //     reg.setConfirmation(record._id)
+  //     sendNotification(record)
 
-      return res.redirect('/register/thankyou?confirm=' + record._id.toHexString())
-    })
-  })
+  //     return res.redirect('/register/thankyou?confirm=' + record._id.toHexString())
+  //   })
+  // })
 
-  app.post('/register', function(req, res) { 
-    ocrypto.httpsCheck(req,res)
-    var post = req.body;
-    var worked = true;
+  // app.post('/register', function(req, res) { 
+  //   ocrypto.httpsCheck(req,res)
+  //   var post = req.body;
+  //   var worked = true;
 
-    var broke = []
-    if (!((post.contact || '').trim()))
-      broke.push({field: 'contact', message: 'required'})
-    if (!((post.street || '').trim()))
-      broke.push({field: 'street', message: 'required'})
-    if (!((post.city || '').trim()))
-      broke.push({field: 'city', message: 'required'})
-    if (!((post.state || '').trim()))
-      broke.push({field: 'state', message: 'required'})
-    if (!((post.zip || '').trim()))
-      broke.push({field: 'zip', message: 'required'})
-    if (!((post.phone || '').trim()))
-      broke.push({field: 'phone', message: 'required'})
-    if (!((post.email || '').trim()))
-      broke.push({field: 'email', message: 'required'})
+  //   var broke = []
+  //   if (!((post.contact || '').trim()))
+  //     broke.push({field: 'contact', message: 'required'})
+  //   if (!((post.street || '').trim()))
+  //     broke.push({field: 'street', message: 'required'})
+  //   if (!((post.city || '').trim()))
+  //     broke.push({field: 'city', message: 'required'})
+  //   if (!((post.state || '').trim()))
+  //     broke.push({field: 'state', message: 'required'})
+  //   if (!((post.zip || '').trim()))
+  //     broke.push({field: 'zip', message: 'required'})
+  //   if (!((post.phone || '').trim()))
+  //     broke.push({field: 'phone', message: 'required'})
+  //   if (!((post.email || '').trim()))
+  //     broke.push({field: 'email', message: 'required'})
 
-    var r = reg.new()
-    r.order.level = parseInt(post.level)
-    r.order.extraSeats = parseInt(post.extra || '0')
-    r.order.donation = parseFloat(post.donation || '0')
+  //   var r = reg.new()
+  //   r.order.level = parseInt(post.level)
+  //   r.order.extraSeats = parseInt(post.extra || '0')
+  //   r.order.donation = parseFloat(post.donation || '0')
 
-    if (r.order.extraSeats < 0)
-      broke.push({field: 'extra', message : 'Must be greater than 0.'})
-    if (r.order.donation < 0)
-      broke.push({field: 'donation', message : 'Must be greater than 0.'})
+  //   if (r.order.extraSeats < 0)
+  //     broke.push({field: 'extra', message : 'Must be greater than 0.'})
+  //   if (r.order.donation < 0)
+  //     broke.push({field: 'donation', message : 'Must be greater than 0.'})
 
-    if (broke.length > 0) {
-      var model = {
-        success : false,
-        broke : broke
-      }
-      res.json(model)
-      return
-    }
+  //   if (broke.length > 0) {
+  //     var model = {
+  //       success : false,
+  //       broke : broke
+  //     }
+  //     res.json(model)
+  //     return
+  //   }
 
-    //TODO: later we can re-organize this and place the pricing into the db.
-    r.order.total += r.order.donation
-    r.order.total += reg.getExtraSeatsAmount(r.order.extraSeats)
-    r.order.total += reg.getSponsorshipAmount(r.order.level)
+  //   //TODO: later we can re-organize this and place the pricing into the db.
+  //   r.order.total += r.order.donation
+  //   r.order.total += reg.getExtraSeatsAmount(r.order.extraSeats)
+  //   r.order.total += reg.getSponsorshipAmount(r.order.level)
 
-    r.contact.company = post.company
-    r.contact.contact = post.contact
-    r.contact.street = post.street
-    r.contact.city = post.city
-    r.contact.state = post.state
-    r.contact.zip = post.zip
-    r.contact.phone = post.phone
-    r.contact.fax = post.fax
-    r.contact.email = post.email
-    r.order.myoTaxCredit = post.myoTaxCreditInfo ? true : false
-    r.order.companyMatch = post.companyMatch ? true : false
-    r.order.boardMember = post.boardMember
-    r.order.seating = post.seating
-    r.order.paymentOption = post.paymentOption || 'check'
+  //   r.contact.company = post.company
+  //   r.contact.contact = post.contact
+  //   r.contact.street = post.street
+  //   r.contact.city = post.city
+  //   r.contact.state = post.state
+  //   r.contact.zip = post.zip
+  //   r.contact.phone = post.phone
+  //   r.contact.fax = post.fax
+  //   r.contact.email = post.email
+  //   r.order.myoTaxCredit = post.myoTaxCreditInfo ? true : false
+  //   r.order.companyMatch = post.companyMatch ? true : false
+  //   r.order.boardMember = post.boardMember
+  //   r.order.seating = post.seating
+  //   r.order.paymentOption = post.paymentOption || 'check'
 
-    reg.insert(r, function(err,data) {
-      if (err) {
-        console.log(err);
-        return res.json(200, {success: false, broke : [], message : 'Unable register at this time. Please try again later.'})
-      }
+  //   reg.insert(r, function(err,data) {
+  //     if (err) {
+  //       console.log(err);
+  //       return res.json(200, {success: false, broke : [], message : 'Unable register at this time. Please try again later.'})
+  //     }
 
-      r._id = data._id
-      res.json({success: true, id : r._id.toHexString()})
-    });
-  })
+  //     r._id = data._id
+  //     res.json({success: true, id : r._id.toHexString()})
+  //   });
+  // })
 }
