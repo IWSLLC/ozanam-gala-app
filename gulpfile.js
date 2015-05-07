ec3509ed57396fe282b529c7bbd05ec4bfe4558f
Nathan Bridgewater <nathan@iws.io>
2015-05-06 21:55:18 -0500

    Adding BS less
    Fixing footer binding.
    
    serve-favicon update
    
    Removing favicon
    
    Adding nav border

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp       = require("gulp")
+var seq        = require("run-sequence")
+var del        = require("del")
+var less       = require("gulp-less")
+var concat     = require('gulp-concat');
+var minify_css = require("gulp-minify-css")
+
+gulp.task('default', function(cb) {
+  seq('clean','static','bootstrap', cb)
+})
+
+gulp.task('clean', function(cb) {
+  return del('public/bootstrap',cb)
+})
+
+gulp.task('static', function() {
+  return gulp.src(['public/bower_components/bootstrap/dist/js/bootstrap.min.js'])
+    .pipe(gulp.dest('public/bootstrap/js'))
+})
+
+gulp.task('bootstrap', function() {
+  return gulp.src(["less/index.less"])
+    .pipe(less())
+    .pipe(minify_css())
+    .pipe(concat("bootstrap.min.css"))
+    .pipe(gulp.dest("public/bootstrap/css"))
+});
